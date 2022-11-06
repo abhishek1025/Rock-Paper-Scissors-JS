59471ae7208ea090bee222a275ad0492af295d17
Abhishek Shrestha <np01cp4a210122@islingtoncollege.edu.np>
2022-11-06 20:27:50 +0545

    The reponsive UI of rock paper scissors game is completed

--- JS/app.js
+++ JS/app.js
@@ -1,93 +1,158 @@
-
-alert("Game rules: You have 3 rounds to play. The final result will be displayed in the webpage.")
-alert("Best of luck <3")
+let playerScore = 0;
+let compScore = 0;
+let totalRound = 0;
 
 const options = ["rock", "paper", "scissors"]
+
+const showGameBtn = document.querySelector("#showGameBtn");
+const banner = document.querySelector("header")
+const gameUI = document.querySelector(".gameUI")
+
+gameUI.style.display = "none";
+
+
+//rock paper game btns
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
+
+//display the selection of com and player btns
+const playerChoiceEL = document.querySelector(".player-choice")
+const computerChoiceEL = document.querySelector(".comp-choice")
+
+//player and computer score elemets
+const playerScoreEl = document.querySelector(".player-score")
+const compScoreEl = document.querySelector(".comp-score")
+
+playerScoreEl.innerHTML = `You : ${playerScore}`;
+compScoreEl.innerHTML = `Computer : ${compScore}`;
+
+//getting the element to displat round result
+const eachRoundResult = document.querySelector(".round-result");
+
+
+
+//getting the elements of pop-up messages
+const popUpMsgSection = document.querySelector(".pop-up-msg");
+const finalScore = document.querySelector("#finalScore");
+
+popUpMsgSection.style.display = "none";
+
+
+
+
+//Hiding the home section and displaying the game UI
+showGameBtn.addEventListener('click', () => {
+    gameUI.style.display = "block";
+    banner.style.display = "none";
+})
+
+//Adding functionality to the btns
+rock.addEventListener("click", () => {
+    playerChoiceEL.innerHTML = `<img src="img/rock.png" alt="Rock">`;
+    const computerChoice = getComputerSelection();
+    playRound("rock", computerChoice);
+})
+
+paper.addEventListener("click", () => {
+    playerChoiceEL.innerHTML = `<img src="img/paper.png" alt="paper">`;
+    const computerChoice = getComputerSelection();
+    playRound("paper", computerChoice);
+})
+scissors.addEventListener("click", () => {
+    playerChoiceEL.innerHTML = `<img src="img/scissors.png" alt="scissors">`;
+    const computerChoice = getComputerSelection();
+    playRound("scissors", computerChoice);
+})
+
+
+
+
 // geting the computer choice
 function getComputerSelection() {
     let index = Math.floor(Math.random() * 3);
-    return options[index];
-}
 
-//getting player's choice
-function getPlayerSelection() {
+    if (options[index] === "rock") {
+        computerChoiceEL.innerHTML = `<img src="img/rock.png" alt="Rock">`;
+        return options[index]
+    }
+    else if (options[index] === "paper") {
+        computerChoiceEL.innerHTML = `<img src="img/paper.png" alt="paper">`;
+        return options[index]
+    }
+    else if (options[index] === "scissors") {
+        computerChoiceEL.innerHTML = `<img src="img/scissors.png" alt="scissors">`;
+        return options[index]
+    }
 
-    return prompt("Enter your Choice: ").toLowerCase();
 }
 
 
 //playing
-function playRound() {
-    const computerChoice = getComputerSelection();
-    const playerChoice = getPlayerSelection().trim();
+function playRound(playerChoice, computerChoice) {
 
-    if (computerChoice === "scissors" && playerChoice === "paper" || computerChoice === "paper" && playerChoice === "rock" || computerChoice === "rock" && playerChoice === "scissors") {
-        return 0;
+    eachRoundResult.style.visibility = "visible";
+
+    //Condition when comp wins
+    if (computerChoice === "scissors" && playerChoice === "paper" ||
+        computerChoice === "paper" && playerChoice === "rock" ||
+        computerChoice === "rock" && playerChoice === "scissors") {
+
+        compScore++;
+        compScoreEl.innerHTML = `Computer : ${compScore}`;
+
+        eachRoundResult.innerHTML = "Computer Wins";
     }
-    else if (computerChoice === "paper" && playerChoice === "scissors" || computerChoice === "rock" && playerChoice === "paper" || computerChoice === "scissors" && playerChoice === "paper") {
-        return 1;
+    //condition when player wins
+    else if (computerChoice === "paper" && playerChoice === "scissors" ||
+        computerChoice === "rock" && playerChoice === "paper" ||
+        computerChoice === "scissors" && playerChoice === "rock") {
+
+        playerScore++;
+        playerScoreEl.innerHTML = `You : ${playerScore}`;
+
+        eachRoundResult.innerHTML = "Player Wins";
+
+
     }
+    //Draw
     else if (computerChoice === "scissors" && playerChoice === "scissors" || computerChoice === "paper" && playerChoice === "paper" || computerChoice === "rock" && playerChoice === "rock") {
-        return 2;
-    }
-    else {
-        return 3;
+        eachRoundResult.innerHTML = "Draw";
     }
 
+    totalRound++;
+
+    restart();
 
 }
 
-
-function game() {
-
-    let playerScore = 0;
-    let compScore = 0;
-    let draw = 0
-
-    for (let i = 0; i < 3; i++) {
-
-        let result = playRound();
-
-        if (result === 0) {
-            compScore++;
-            alert("Comp wins")
-        } else if (result === 1) {
-            alert("Player wins")
-            playerScore++;
-        }
-        else if (result == 2) {
-            alert("Draw")
-            draw++;
-        }
-        else {
-
-            break;
-        }
-
-    }
-
-    if (playerScore > compScore) {
-        document.write(`Player wins by ${playerScore} - ${compScore} <br>`);
-    }
-    else if (compScore > playerScore) {
-        document.write(`Computer wins by ${compScore} - ${playerScore} <br>`);
-    }
-
-    document.write(`Draw ${draw} games <hr>`);
-    restart()
-
-}
 
 function restart() {
 
-    const askUser = prompt("Want to play again? y/n: ").toLowerCase();
+    if (totalRound == 5) {
 
-    if (askUser === "y") {
-        game()
+        finalScore.innerHTML = `(You) ${playerScore} - ${compScore} (Computer)`;
+
+        playerScore = 0;
+        compScore = 0;
+        totalRound = 0;
+
+        compScoreEl.innerHTML = `Computer : ${compScore}`;
+        playerScoreEl.innerHTML = `You : ${playerScore}`;
+
+        computerChoiceEL.innerHTML = "";
+        playerChoiceEL.innerHTML = "";
+
+        popUpMsgSection.style.display = "block";
+
     }
-
+    else {
+        popUpMsgSection.style.display = "none";
+    }
 }
 
-game()
+
+
+
 
 
